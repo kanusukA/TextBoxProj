8d1fa671def07505b8d8dc85e1b2104730421340
kanusukA <aayushkaushal74@gmail.com>
2026-01-30 21:36:49 +0530

    Fixed multiline extension and animations.
    The width still has to be defined and is not dynamic.

--- textboxproj/lib/TextBoxPaint.dart
+++ textboxproj/lib/TextBoxPaint.dart
@@ -1,5 +1,5 @@
-import 'package:espy/Views/Components/SubModules/textboxproj/lib/boxPainter.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class MyTextBox extends StatelessWidget {
   const MyTextBox({super.key});
@@ -105,7 +105,7 @@ class _SentientTextFieldState extends State<SentientTextField> with SingleTicker
 
     // _animation = Tween<double>(begin: 0, end: widget.indentHeight).animate(_animationController);
 
-    _animatedIndentHeightTextBox = Tween<double>(begin: 0, end: widget.indentHeight).animate(CurvedAnimation(parent: _animationController, curve: Curves.elasticInOut));
+    _animatedIndentHeightTextBox = Tween<double>(begin: 0, end: widget.indentHeight + increaseHeight).animate(CurvedAnimation(parent: _animationController, curve: Curves.elasticInOut));
 
     super.initState();
   }
@@ -166,7 +166,7 @@ class _SentientTextFieldState extends State<SentientTextField> with SingleTicker
                   },
                   onExapaned: (height) {
                     setState(() {
-                      increaseHeight = height;
+                      increaseHeight = height - (widget.size.height - 16);
                     });
                   },
                 ),
@@ -226,7 +226,7 @@ class __STextFieldState extends State<_STextField> {
       decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: widget.textBoxColor),
       child: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 12),
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
           child: TextField(
             minLines: 1,
             maxLines: null,
@@ -249,4 +249,108 @@ class __STextFieldState extends State<_STextField> {
       ),
     );
   }
+}
+
+// PAINTER
+
+class TextBoxPainter extends CustomPainter {
+  double cornerRadius = 0;
+  double indentSpaceFromStart = 0;
+  double indentWidth = 0;
+  double indentHeight = 0;
+  double indentCornerSize = 0;
+  double indentCornerRadius = 0;
+  Color color = Colors.deepPurple;
+  String? text;
+  TextStyle? style;
+
+  TextBoxPainter(
+    double cornerRadius_p,
+    double indentSpaceFromStart_p,
+    double indentWidth_p,
+    double indentHeight_p,
+    double indentCornerSize_p,
+    double indentCornerRadius_p,
+    Color col,
+    String? text_p,
+    TextStyle? style_p,
+  ) {
+    cornerRadius = cornerRadius_p;
+    indentSpaceFromStart = indentSpaceFromStart_p;
+    indentWidth = indentWidth_p;
+    indentHeight = indentHeight_p;
+    indentCornerSize = indentCornerSize_p;
+    indentCornerRadius = indentCornerRadius_p;
+    color = col;
+    text = text_p;
+    style = style_p;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 4;
+    paint.color = color;
+
+    Path path = TextBoxPath(size, cornerRadius, indentSpaceFromStart, indentWidth, indentHeight, indentCornerRadius, indentCornerSize);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return false;
+  }
+}
+
+Path TextBoxPath(Size size, double cornerRadius, double _indentSpaceFromStart, double indentWidth, double indentHeight, double indentCornerRadius, double indentCornerSize) {
+  var path = Path();
+
+  var indentSpaceFromStart = (cornerRadius / 2) + _indentSpaceFromStart;
+
+  path.moveTo(cornerRadius / 2, 0);
+
+  // Top Start ARC
+  // path.arcTo(Rect.fromPoints(Offset.zero, Offset(cornerRadius, cornerRadius)), math.pi / 2, math.pi, false);
+
+  path.lineTo(indentSpaceFromStart, 0);
+
+  path.cubicTo(
+    indentSpaceFromStart + indentCornerRadius,
+    0,
+    (indentSpaceFromStart + (indentCornerSize * 2)) - indentCornerRadius,
+    indentHeight,
+    indentSpaceFromStart + (indentCornerSize * 2),
+    indentHeight,
+  );
+
+  path.lineTo(indentWidth + indentSpaceFromStart + (indentCornerSize * 2), indentHeight);
+
+  path.cubicTo(
+    indentSpaceFromStart + (indentCornerSize * 2) + indentWidth + indentCornerRadius,
+    indentHeight,
+    (indentSpaceFromStart + indentWidth + (indentCornerSize * 4)) - indentCornerRadius,
+    0,
+    indentSpaceFromStart + (indentCornerSize * 4) + indentWidth,
+    0,
+  );
+
+  path.lineTo(size.width - cornerRadius / 2, 0);
+
+  // // Top End Arc
+  path.arcTo(Rect.fromPoints(Offset(size.width - cornerRadius, 0), Offset(size.width, cornerRadius)), -(math.pi / 2), math.pi / 2, false);
+
+  path.arcTo(Rect.fromPoints(Offset(size.width - cornerRadius, size.height - cornerRadius), Offset(size.width, size.height)), math.pi * 2, math.pi / 2, false);
+
+  path.lineTo(cornerRadius / 2, size.height);
+
+  path.arcTo(Rect.fromPoints(Offset(0, size.height - cornerRadius), Offset(cornerRadius, size.height)), math.pi / 2, math.pi / 2, false);
+
+  path.arcTo(Rect.fromPoints(Offset.zero, Offset(cornerRadius, cornerRadius)), math.pi, math.pi / 2, false);
+
+  path.close();
+
+  return path;
 }
